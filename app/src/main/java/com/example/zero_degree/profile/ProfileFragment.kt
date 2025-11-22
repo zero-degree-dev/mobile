9518e78306158b42cf16310dce61b020182cba3b
VanVaVa <MisterDeterminator@yandex.ru>
2025-11-22 14:37:14 +0000

    feat: added screens

---
+++ app/src/main/java/com/example/zero_degree/profile/ProfileFragment.kt
@@ -0,0 +1,18 @@
+package com.example.zero_degree.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.zero_degree.R
+
+class ProfileFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.profile_screen, container, false)
+    }
+}
